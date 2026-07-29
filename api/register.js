145d4fda261c3c843404d2a361106d28dae10247
citerboy839-ops <citerboy839@gmail.com>
2026-07-29 21:22:50 +0700

    Delete api directory

--- api/register.js
+++
@@ -1,4 +0,0 @@
-export default async function handler(req, res) {
-    // Cuma return sukses tanpa logic rumit
-    return res.status(200).json({ success: true, message: 'OK' });
-}
--- api/status.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-    return res.status(200).json({
-        address: 'publicserver1.pteroweb.biz.id:3000',
-        online: true,
-        lastSeen: Date.now()
-    });
-}
